4a625f6f2c54b903798914dedd0e0c257272c86d
Hitesh Patel <hpind7117@gmail.com>
2018-02-10 00:54:35 -0500

    First commit

---
+++ StaticClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StaticClass
+{
+    public static class Logger
+    {
+        public static readonly object lockobject=new object();
+        public static string filepath { get; set; }
+
+        public static  void log(string msg)
+        {
+            lock (lockobject)
+            {
+                if (!string.IsNullOrEmpty(filepath))
+                {
+                    using (StreamWriter sw = new StreamWriter(filepath, true))
+                    {
+                        sw.WriteLine(msg);
+                        sw.Close();
+                    }
+                }
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.filepath = "D:\\a.txt";
+            Logger.log ( "Sai Ram");
+            Console.Read();
+        }
+    }
+}
